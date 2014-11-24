0fae3480828828a679849401e9a433948bf9284f
Jacob Schmitz <jake.schmitz101@gmail.com>
2014-11-24 16:58:02 -0500

    first version that is making successful requests and properly reading titles

--- scanssl.py
+++ scanssl.py
@@ -11,29 +11,29 @@ class doc:
     self.source = source
 
   def to_csv(self, delim=','):
-    print self.doc_id + delim + self.title + delim + self.authors + delim + self.source
+    str(self.doc_id) + delim + self.title + delim + self.authors + delim + self.source
 
 base_url = 'http://127.0.0.1:1025/gsdl?a=d&d=D'
 # articles is a map from doc ids to an instance of a doc 
 # class
 articles = []
 
-for i in xrange(9999):
-  page = urllib.request.urlopen(base_url + str(i))
+for i in range(9999):
+  page = urllib.request.urlopen(base_url + str(i)).read().decode('utf-8')
   # the title is in the only h3 tag
   chunks = page.split('h3>')
   # remove part of closing h3 tag from title
   title = chunks[1].replace('</', '')
-  print title
+  print(title)
   # Read the html of one of the pages and this will make sense
   # It would be cleaner if people knew how to use ids in html
-  auts = chunks.last.split('Author(s)</td>')[0].split('<td bgcolor="#E6F0FF">')[0].split('</td>')[0]
-  src = chunks.last.split('Source</td>')[0].split('<td bgcolor="#E6F0FF">')[0].split('</td>')[0]
+  auts = chunks[-1].split('Author(s)</td>')[0].split('<td bgcolor="#E6F0FF">')[0].split('</td>')[0]
+  src = chunks[-1].split('Source</td>')[0].split('<td bgcolor="#E6F0FF">')[0].split('</td>')[0]
   article = doc(i, title, auts, src)
-  print article.to_csv()
-  articles << article
+  print(article.to_csv())
+  articles.append(article)
   # Check if this article already has a valid link/ source
-  #if chunks.last.find('[Article not available]') >= 0:
+  #if chunks[-1].find('[Article not available]') >= 0:
     # This article does not have a source available
     #print doc.to_csv() 
   
