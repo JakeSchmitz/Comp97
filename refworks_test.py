cc5dc95e962675c49027b640b3ca3d9c03d13c59
Ashley Hedberg <ashley.hedberg@tufts.edu>
2015-01-19 18:31:50 -0500

    reindented

--- refworks_test.py
+++ refworks_test.py
@@ -11,41 +11,39 @@ from secrets import *
 
 
 def generate_signature(class_name, access_key, secret_key):
-    expires = int(time.mktime(datetime.datetime.now().timetuple())) * 1000
-    raw_signature = class_name + access_key + str(expires)
-    signature = hmac.new(secret_key, raw_signature,
-                         sha1).digest().encode('base64').rstrip('\n')
-    return {'class': class_name, 'accesskeyid': access_key, 'expires': expires,
-            'signature': signature}
+  expires = int(time.mktime(datetime.datetime.now().timetuple())) * 1000
+  raw_signature = class_name + access_key + str(expires)
+  signature = hmac.new(secret_key, raw_signature, sha1).digest().encode('base64').rstrip('\n')
+  return {'class': class_name, 'accesskeyid': access_key, 'expires': expires, 'signature': signature}
 
 
 
 def make_session():
-    base_url = 'https://www.refworks.com/api2/'
-    params = generate_signature('authentication', ACCESS_KEY_ID, SECRET_ID)
-    params.update({'method': 'newsess'})
-    xml = ('<AcctInfo>'
-           '<groupCode>%s</groupCode>'
-           '<loginName>%s</loginName>'
-           '<password>%s</password>'
-           '</AcctInfo>') % (GROUP_CODE, USERNAME, PASSWORD)
-    r = requests.post(base_url, params=params, data=xml)
-    print r.text
+  base_url = 'https://www.refworks.com/api2/'
+  params = generate_signature('authentication', ACCESS_KEY_ID, SECRET_ID)
+  params.update({'method': 'newsess'})
+  xml = ('<AcctInfo>'
+         '<groupCode>%s</groupCode>'
+         '<loginName>%s</loginName>'
+         '<password>%s</password>'
+         '</AcctInfo>') % (GROUP_CODE, USERNAME, PASSWORD)
+  r = requests.post(base_url, params=params, data=xml)
+  print r.text
 
 
 def init_logging():
-    httplib.HTTPConnection.debuglevel = 1
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.DEBUG)
-    requests_log = logging.getLogger('requests.packages.urllib3')
-    requests_log.setLevel(logging.DEBUG)
-    requests_log.propagate = True
+  httplib.HTTPConnection.debuglevel = 1
+  logging.basicConfig()
+  logging.getLogger().setLevel(logging.DEBUG)
+  requests_log = logging.getLogger('requests.packages.urllib3')
+  requests_log.setLevel(logging.DEBUG)
+  requests_log.propagate = True
 
 
 def main():
-    #init_logging()
-    make_session()
+  #init_logging()
+  make_session()
 
 
 if __name__ == '__main__':
-    main()
+  main()
