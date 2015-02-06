29df135e1e2c6256407cbbdb457203184a377dec
Ashley Hedberg <ashley.hedberg@tufts.edu>
2015-02-06 12:58:17 -0500

    discovered we only need one of the cookies

--- tischlibrary_test.py
+++ tischlibrary_test.py
@@ -4,11 +4,11 @@ import requests
 
 c = {}
 c['Summon-Two'] = 'TRUE'
-c['__utma'] = '160199096.1776388197.1421705704.1423013448.1423184948.4'
-c['__utmc'] = '160199096'
-c['__utmz'] = '160199096.1423184948.4.4.utmcsr=tischlibrary.tufts.edu|utmccn=(referral)|utmcmd=referral|utmcct=/'
+#c['__utma'] = '160199096.1776388197.1421705704.1423013448.1423184948.4'
+#c['__utmc'] = '160199096'
+#c['__utmz'] = '160199096.1423184948.4.4.utmcsr=tischlibrary.tufts.edu|utmccn=(referral)|utmcmd=referral|utmcct=/'
 #c['hasSavedItems'] = 1
 
 r = requests.get('http://tufts.summon.serialssolutions.com/api/search?pn=1&ho=t&q=The+Localization+of+Development+in+Comparative+Perspective', cookies=c)
 
-print r.text
+print r.json()
