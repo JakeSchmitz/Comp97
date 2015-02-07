e4599c7806bf0ebfdb143d72c1370379681f1643
Ashley Hedberg <ashley.hedberg@tufts.edu>
2015-02-07 14:16:56 -0500

    tested 100 random articles from ssl data

--- tischlibrary_test.py
+++ tischlibrary_test.py
@@ -1,14 +1,55 @@
 """This tests the scraping of JSON search results from the Tisch Library website."""
 
+import csv
+import json
+import random
 import requests
+import sys
 
-c = {}
-c['Summon-Two'] = 'TRUE'
-#c['__utma'] = '160199096.1776388197.1421705704.1423013448.1423184948.4'
-#c['__utmc'] = '160199096'
-#c['__utmz'] = '160199096.1423184948.4.4.utmcsr=tischlibrary.tufts.edu|utmccn=(referral)|utmcmd=referral|utmcct=/'
-#c['hasSavedItems'] = 1
 
-r = requests.get('http://tufts.summon.serialssolutions.com/api/search?pn=1&ho=t&q=The+Localization+of+Development+in+Comparative+Perspective', cookies=c)
+COOKIE = {'Summon-Two': 'TRUE'}
+BASE_URL = 'http://tufts.summon.serialssolutions.com/api/search?pn=1&ho=t&q='
+CSV_FILE = 'ssl-data/ssldata.csv'
 
-print r.json()
+
+def retrieve_search_results(title):
+  """Sends a query for items with the given title to the Tisch Library website."""
+  r = requests.get(BASE_URL + title, cookies=COOKIE)
+  try:
+    content = json.loads(r.text)
+  except:
+    sys.stderr.write('ERROR: Results for "%s" could not be loaded to JSON.\n' % title)
+  else:
+    try:
+      first_document = content['documents'][0]
+    except:
+      sys.stderr.write('ERROR: No results for "%s".\n' % title)
+    else:
+      try:
+        # print json.dumps(first_document, sort_keys=True, indent=4, separators=(',', ': '))
+        print first_document['title'].replace('<b>', '').replace('</b>', '')
+      except:
+        sys.stderr.write('ERROR: Could not get title for "%s".\n' % title)
+
+
+def main():
+  """Tests random articles from the SSL to make sure we can get valid output."""
+  title_list = []
+
+  with open(CSV_FILE, 'rU') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+      # title is the second entry in csv file
+      title_list.append(row[1])
+
+  sample = random.sample(xrange(len(title_list)), 100)
+  for i in sample:
+    if title_list[i] == 'SCRAPING ERROR':
+      continue
+    print title_list[i]
+    retrieve_search_results(title_list[i])
+    print ''
+
+
+if __name__ == '__main__':
+  main()
