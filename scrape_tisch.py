dbb4544f34ff8e97169f9da8b1a62b5fc0b1f128
Ashley Hedberg <ashley.hedberg@gmail.com>
2015-02-12 13:40:24 -0500

    refactored and hopefully fixed bugs that made it crash

--- scrape_tisch.py
+++ scrape_tisch.py
@@ -12,6 +12,49 @@ BASE_URL = 'http://tufts.summon.serialssolutions.com/api/search?pn=1&ho=t&q='
 SSL_CSV_FILE = 'isbnscrape.csv'
 
 
+def compare_isbn(entry, doc):
+  """Use ISBNs to determine if doc is the entry from SSL."""
+  doc_isbns = []
+  try:
+    doc_isbns += doc['issns']
+  except KeyError as e:
+    print('%s %s: %s\n' % (entry['id'], entry['title'], e))
+  try:
+    doc_isbns += doc['eissns']
+  except KeyError as e:
+    print('%s %s: %s\n' % (entry['id'], entry['title'], e))
+  for doc_isbn in doc_isbns:
+    if entry['isbn'] == doc_isbn:
+      # Found match
+      return True
+  return False
+
+
+def compare_title_author(entry, doc):
+  """Use title and author to determine if doc is entry from SSL."""
+  try:
+    doc_title = doc['title'].replace('<b>', '').replace('</b>', '').upper()
+    ssl_title = entry['title'].upper()
+    re.sub(r'\W+', '', doc_title)
+    re.sub(r'\W+', '', ssl_title)
+    # Check titles
+    if (doc_title != ssl_title) and (doc_title not in ssl_title) and (ssl_title not in doc_title):
+      return False
+    # Check authors
+    for doc_author in doc['authors']:
+      author_parts = doc_author['fullname'].split()
+      for part in author_parts:
+        re.sub(r'\W+', '', part)
+        if part not in entry['authors']:
+          return False
+    # All authors from Tisch are in the SSL entry
+    # TODO: Should we mark the document as a match if all of one of the authors match?
+    return True
+  except KeyError as e:
+    # Some entry we tried to use doesn't exist
+    print('%d %s: %s\n' % (entry['id'], entry['title'], e))
+  return False
+
 def retrieve_search_results(entry, writer):
   """Sends a query for items with the given title to the Tisch Library website."""
   r = requests.get(BASE_URL + entry['title'], cookies=COOKIE)
@@ -20,56 +63,23 @@ def retrieve_search_results(entry, writer):
   try:
     full_search_results = r.json()
   except ValueError as e:
-    print('%d %s: %s\n' % (entry['id'], entry['title'], e))
-    writer.writerow([entry['id'], entry['browsepath'], entry['title'], entry['authors'], entry['isbn'], None])
+    print('%d %s: %s' % (entry['id'], entry['title'], e))
     return
 
   # Determine if there are any documents to deal with
   documents = full_search_results['documents']
   if len(documents) == 0:
-    print('%d %s: No results found on Tisch Library\n' % (entry['id'], entry['title']))
-    writer.writerow([entry['id'], entry['browsepath'], entry['title'], entry['authors'], entry['isbn'], None])
+    print('%d %s: No results found on Tisch Library' % (entry['id'], entry['title']))
     return
 
   # Figure out if any of them are the one we want
   for doc in documents:
-    # Check ISBN
-    doc_isbns = []
-    try:
-      doc_isbns += doc['issns']
-    except KeyError:
-      pass
-    try:
-      doc_isbns += doc['eissns']
-    except KeyError:
-      pass
-    for doc_isbn in doc_isbns:
-      if entry['isbn'] == doc_isbn:
-        # Found match
-        writer.writerow([entry['id'], entry['browsepath'], entry['title'], entry['authors'], entry['isbn'], doc])
-        return
-    # If no ISBN match, check titles by comparing only uppercase alpha(numeric) characters
-    try:
-      doc_title = doc['title'].replace('<b>', '').replace('</b>', '').upper()
-      ssl_title = entry['title'].upper()
-      re.sub(r'\W+', '', doc_title)
-      re.sub(r'\W+', '', ssl_title)
-      if doc_title != ssl_title:
-        continue
-      # Check authors
-      for doc_author in doc['authors']:
-        doc_author_last, doc_author_first = doc_author['fullname'].strip().split(',')
-      if (doc_author_last in entry['authors']) and (doc_author_first in entry['authors']):
-        # Found match
-        writer.writerow([entry['id'], entry['browsepath'], entry['title'], entry['authors'], entry['isbn'], doc])
-        return
-    except KeyError as e:
-      # Some entry we tried to use doesn't exist
-      print('%d %s: %s\n' % (entry['id'], entry['title'], e))
+    if compare_isbn(entry, doc) or compare_title_author(entry, doc):
+      writer.writerow([entry['id'], entry['browsepath'], entry['title'], entry['authors'], entry['isbn'], doc])
+      return
 
   # No documents caused a match
-  print('%d %s: No results matched on Tisch Library\n' % (entry['id'], entry['title']))
-  writer.writerow([entry['id'], entry['browsepath'], entry['title'], entry['authors'], entry['isbn'], None])
+  print('%d %s: No results matched on Tisch Library' % (entry['id'], entry['title']))
 
 
 def read_input():
