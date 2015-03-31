76db09ef31bf5674f649d73a364b6a5e73699299
Jacob Schmitz <jake.schmitz101@gmail.com>
2015-03-31 09:04:28 -0400

    fixing unicode strings

--- mongowriter.py
+++ mongowriter.py
@@ -1,5 +1,7 @@
 import sys
+import re
 import pymongo
+import math
 from pymongo import MongoClient
 import csv
 import json
@@ -17,21 +19,42 @@ class MongoWriter:
         if rowcap is not None and rowcap < i:
           break
         self.data[row[0]] = dict()
-        self.data[row[0]]['id'] = row[0]
+        doc_id = str(row[0])
+        if int(int(row[0]) / 1000) == 0 and row[0] != '0':
+          magnitude = int(math.floor(math.log10(abs(float(doc_id)))))
+          for i in range(3 - magnitude):
+            doc_id = '0' + doc_id
+        elif row[0] == '0':
+          doc_id = '0000'
+        self.data[row[0]]['_id'] = str(doc_id)
         self.data[row[0]]['browsepath'] = row[1] 
         self.data[row[0]]['ssltitle'] = row[2]
         self.data[row[0]]['sslauthor'] = row[3]
         self.data[row[0]]['isbn'] = row[4]
         for c in row[5:]:
           try: 
-            broken = c.decode('utf-8').split(',')
+            broken = c.split(',')
             k = '('.join(broken[0].split('(')[1:])
             v = ','.join(broken[1:]) if len(broken[1:]) > 1 else broken[1]
             v = ')'.join(v.split(')')[:-1])
-            self.data[row[0]][k] = v.encode('utf-8')
+            v = re.split('u[\'|\"]', v)
+            if len(v) > 1:
+              map(lambda word: word[:-1], v)
+            val = []
+            for x in v:
+              if x is not None and x is not '':
+                newx = x.replace('\'', '')
+                if newx[0] == ' ' and len(newx) > 1:
+                  newx = newx[1:]
+                val.append(newx)
+            if k.startswith('u\''):
+              k = k[2:-1]
+            val = ''.join(val)
+            self.data[row[0]][k.encode('ascii', 'ignore')] = val.encode('ascii', 'ignore')
           except:
-            print 'messed up tuple dissection'
-  
+            print sys.exc_info()[1]
+        self.data[row[0]]['id'] = str(doc_id)
+
   def test_write(self, num_rows=1):
     i = 0
     for k in self.data.keys():
@@ -41,12 +64,16 @@ class MongoWriter:
         break
 
   def write_all_data(self):
-    failures = []
+    failures = {}
     for k in self.data.keys():
       try:
-        self.collection.insert(self.data[k])
-      except: 
-        failures.append(self.data[k]['id'])
+        self.collection.save(self.data[k])
+      except pymongo.errors.DuplicateKeyError: 
+        failures[self.data[k]['_id']] = sys.exc_info()[1]
+        print sys.exc_info()[1]
+      except:
+        failures[self.data[k]['_id']] = sys.exc_info()[1]
+        print sys.exc_info()[1]
     return failures
  #     except:
   #      print 'Failed to write record to mongo ' + str(self.data[k]['id'])
